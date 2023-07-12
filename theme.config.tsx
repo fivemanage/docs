d1fafcbf10971c3e0ac299377b839a2251093f7a
itschip <chip@projecterror.dev>
2023-07-12 22:09:44 +0200

    initial docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  primaryHue: {
+    dark: 30,
+    light: 30,
+  },
+  logo: <span className="nx-font-bold">Fivemanage</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/fivemanage/docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/wUgAGHnucZ',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/fivemanage/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Fivemanage Docs',
   },
 }
 
